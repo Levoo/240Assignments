6e7b7be93a97e143e533bdebc765e9fea4b66093
Justin Huang <justin_huang@csu.fullerton.edu>
2017-09-06 23:53:27 -0700

    Update assignment2.cpp

--- assignment2.cpp
+++ assignment2.cpp
@@ -57,7 +57,151 @@ int main()
 	//-------------------END QUESTION 1------------------------
 
 	//-------------------START QUESTION 2----------------------
+	
+	#include <iostream>
+using namespace std;
 
+
+
+int main()
+{
+	int a, b, c, h, l, w, areaT, preT, areaR, preR;
+	int t = 2;
+	cout << "    /| \ " << endl; 
+	cout << "   / |  \ " << endl; 
+	cout << "a /  |   \ b" << endl; 
+	cout << " /  h|    \ " << endl; 
+	cout << "/    |     \ " << endl; 
+	cout << "____________ " << endl; 
+	cout << "      c      " << endl; 
+
+	cout << " -------------------------- " << endl; 
+	cout << " |                        |" << endl; 
+	cout << " |                        | width" << endl;
+	cout << " |                        |" << endl;
+	cout << " |                        |" << endl;
+	cout << " --------------------------" << endl;
+	cout << "             length         " << endl;
+	
+
+	cout << " Please enter for a,b,c and h" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> h; 
+
+	cout << " Please enter for length and width " << endl;
+	cin >> l; 
+	cin >> w; 
+
+	_asm
+	{
+		mov eax,c 
+		imul h
+		idiv t 
+		mov areaT,eax
+
+		mov eax,c 
+		add eax,a 
+		add eax,b 
+		mov preT,eax
+
+		mov eax,l 
+		imul w 
+		mov areaR, eax
+
+		mov eax,l 
+		add eax,w
+		add eax,eax
+		mov preR,eax
+			}
+
+	cout << "triangle " << endl;
+	cout << "Area............." << areaT << endl;
+	cout << "Perimeter........" << preT << endl;
+
+	cout << "Rectangle " << endl;
+	cout << " Area............" << areaR << endl; 
+	cout << " Perimeter......." << preR << endl; 
+
+
+
+	return 0;
+}
+
+
+//-------------------------------OutPut---------------------------------------------
+
+//      / |
+//     /  |
+//  a /   | b
+//   / h  |
+//  /     |
+//  ____________
+//          c
+//--------------------------
+//|                        |
+//|                        | width
+//|                        |
+//|                        |
+//--------------------------
+//length
+//Please enter for a, b, c and h
+//4
+//6
+//9
+//10
+//Please enter for length and width
+//15
+//10
+//
+//triangle
+//Area.............45
+//Perimeter........19
+//Rectangle
+//Area............150
+//Perimeter.......50
+//Press any key to continue . . .
+
+
+//---------------------------------------------------------------------------------
+	
+//-------------------QUESTION 3------------------------------------------------------
+	/*******************
+Assignment 2 (ADD, SUB, IMUl, IDIV instructions)
+
+Cuevas, Fernando
+Huang, Justin
+
+*****************/
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	short F, C,b=9,d=5;
+
+	cout << " Enter temperature in Fahrenheight: ";
+	cin >> F; 
+	_asm
+	{
+		mov ax, F
+		sub ax, 32
+		imul b
+		idiv d
+		mov c,ax 
+
+	}
+	cout << F << "F " << C << "C " << endl; 
+
+		return 0; 
+
+
+
+
+}
+	
+// -----------------------------------------------------------------------------------------------
 	//print the menu
 	 
 
