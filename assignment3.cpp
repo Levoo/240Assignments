c58660a073febb2327982676f85c295a363b2b7e
Fernando <fzavala649@gmail.com>
2017-09-09 16:14:45 -0700

    My take on question 1 and 4
    just need 2 and 3 now

--- assignment3.cpp
+++ assignment3.cpp
@@ -7,7 +7,7 @@ Authors: Cuevas, Fernando
 Due: 9/14/2017
 */
 
-#include "stdafx.h" //take out after done with project
+#include "stdafx.h" //take out after done with project, only need it cus my visual studio neeeds it.
 #include <iostream>
 #include <string>
 
@@ -23,57 +23,128 @@ void display711Menu();
 void get711Order();
 void reciptFor711();
 
-//variables for question 2
+//variables and functions for question 2
+int averageScore, scores, scoreCount;
+int stopLoop = -1;
 
+void getScore();
 //variables for question 3
 
 //variables for question 4
+short int a, b, c, d, e, f;
+short int X, Y;
+
+void displayXYMenu();
+void printXYresults();
 
 int main() {
 
 	//-----------------------------------QUESTION 1--------------------------------------------------------
-	__asm {
-	entry:
-		call display711Menu;  display the menu to console
-			call get711Order;
-		whatDrinkType:
-			mov  eax, amtDrink; move the total drinks to eax
-			mov  bl, 'S'; move S into al, this is to not have another variable to compare
-			cmp  userChoiceDrink, bl;  compare what the user entered
-			jne  choiceWater;  if the user entered S then keep going though else go to choiceWater
+	
+	//__asm {
+	//entry:
+	//	call display711Menu;  display the menu to console
+	//		call get711Order;
+	//	whatDrinkType:
+	//		mov  eax, amtDrink; move the total drinks to eax
+	//		mov  bl, 'S'; move S into al, this is to not have another variable to compare
+	//		cmp  userChoiceDrink, bl;  compare what the user entered
+	//		jne  choiceWater;  if the user entered S then keep going though else go to choiceWater
+	//
+	//	choiceSoda : //go here is user chooses soda
+	//		imul sodaPrice;
+	//		mov totalPrice, eax;
+	//		jmp whatSubSize;
+	//
+	//	choiceWater: // else choose water
+	//		imul waterPrice;
+	//		mov totalPrice, eax;
+	//
+	//	whatSubSize: //next part will handle what sub size
+	//		mov eax, amtFood;
+	//		mov  ebx, 10;
+	//		cmp  userChoiceSub, ebx;
+	//		jne  subChoice12;
+	//
+	//	subChoice10:
+	//		imul sub10inchPrice;
+	//		add totalPrice, eax;
+	//		jmp displayRecipte;
+	//
+	//	subChoice12:
+	//		imul sub12inchPrice;
+	//		add totalPrice, eax;
+	//
+	//	displayRecipte:
+	//		call reciptFor711;
+	//
+	//}
 
-		choiceSoda : //go here is user chooses soda
-			imul sodaPrice;
-			mov totalPrice, eax;
-			jmp whatSubSize;
+	//-----------------------------------QUESTION 2--------------------------------------------------------
 
-		choiceWater: // else choose water
-			imul waterPrice;
-			mov totalPrice, eax;
+	/*todo*/
 
-		whatSubSize: //next part will handle what sub size
-			mov eax, amtFood;
-			mov  ebx, 10;
-			cmp  userChoiceSub, ebx;
-			jne  subChoice12;
+	//-----------------------------------QUESTION 3--------------------------------------------------------
 
-		subChoice10:
-			imul sub10inchPrice;
-			add totalPrice, eax;
-			jmp displayRecipte;
+	/*todo*/
+	//-----------------------------------QUESTION 4--------------------------------------------------------
+	/*
+	_asm {
+		call displayXYMenu;
 
-		subChoice12:
-			imul sub12inchPrice;
-			add totalPrice, eax;
+	getY:
+		// find a*f - c * d
+		mov ax, a;
+		imul f;
+		mov bx, ax;
+		mov ax, c;
+		imul d;
+		sub bx, ax;
+		mov cx, bx;
+		// find a*e - b * d
+		mov ax, a; move a in ax multiply by e
+		imul e;
+		mov bx, ax; move ax into bx for stroage 
+		mov ax, b; move b in ax and multiply with d
+		imul d; 
+		sub bx, ax; subtract bx with ax
+		mov ax, cx; move result into cx, which is the numerator
+		// divide numerator with denominato
+		cwd;		convert to double word and then divid 
+		idiv bx;
+		mov Y, ax;
 
-		displayRecipte:
-			call reciptFor711;
+	getX:
+		//find c*e - b*f
+		mov ax, c;
+		imul e;
+		mov bx, ax;
+		mov ax, b;
+		imul f;
+		sub bx, ax;
+		mov cx, bx;
+		//find a*e - b*d
+		mov ax, a;
+		imul e;
+		mov bx, ax;
+		mov ax, b;
+		imul d;
+		sub bx, ax;
+		mov ax, cx;
+		//divide
+		cwd;
+		idiv bx;
+		mov X, ax;
+
+	display:
+		call printXYresults;
 
 	}
-
+	*/
 	return 0;
 }
 
+// functions for question 1
 void display711Menu() {
 	std::cout << "-----------------------------7-11 Convienient Store------------------------------\n";
 	std::cout << " Drinks\n\tSoda(S)...................................$2\n";
@@ -101,4 +172,18 @@ void reciptFor711() {
 	std::cout << " Your total bill = " << totalPrice << std::endl;
 }
 
-//-----------------------------------QUESTION 1--------------------------------------------------------
+
+//function for question 4
+void displayXYMenu() {
+	std::cout << "This program solves the system\n\taX + bY = c\n\tdX + eY = f\n";
+
+	std::cout << "Enter the values of a, b and c: ";
+	std::cin >> a >> b >> c;
+
+	std::cout << "Enter the values of d, e and f: ";
+	std::cin >> d >> e >> f;
+}
+
+void printXYresults() {
+	std::cout << "\tX = " << X << "\n\tY = " << Y << std::endl;
+}
