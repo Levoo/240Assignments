25980a440a9ad9ee9208415981d2452c59480109
Fernando <fzavala649@gmail.com>
2017-10-02 01:42:35 -0700

    Update project6.cpp
    need part c and d

--- project6.cpp
+++ project6.cpp
@@ -36,14 +36,37 @@ int aa[5] = {9, 3, 22, 8, 1};
 
 int loop1Count = 0, loop2Count, index, nextIndex, element;
 
-void displayArrayElement() {
-	std::cout << element << " ";
+// to display the array i get an error when i try to display within assembly after moving the first value into element the value 
+// of ecx changes dramatically which leads to a crash, possible somthing with the call.
+void displayArray() {
+	for (int i = 0; i < 5; i++) {
+		std::cout << aa[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 void displayMsg_Original() {
 	std::cout << "Original Array: ";
 }
 
+//-------------------------QUESTION 3 stuff---------------------
+int aaa[3][3][2] = { 1, 2, 3,
+					 4, 5, 6,
+					 7, 8, 9,
+					 10, 11, 12,
+					 13, 14, 15,
+					 16, 17, 18 };
+int arrayPos = 0, total3DShirtCount = 0, total3DMediumCount = 0, total3DShortSleeve = 0, total3DReadShirtCount = 0;
+int countJump = 4;
+//functsa 
+
+void displayTotal3DCount() {
+	std::cout << "a.) Total # of shirts is: " << total3DShirtCount << std::endl;
+}
+
+void displayTOtalMed() {
+	std::cout << "b.) Total # of shirts is: " << total3DMediumCount << std::endl;
+}
 
 int main() {
 	//part a of 1
@@ -100,32 +123,107 @@ int main() {
 
 	//----------------------------------------------------
 
-	//part 2, having an error displaying the array need to figure out. Ecx value changes # > array size and crashes the program
-  //        removing the 
+	//part 2
+	
+	/*_asm {
+		call displayArray;
+		mov ecx, 0;
+		mov loop1Count, 0;
+		mov loop2Count, 0;
+		jmp b_Sort;
+	
+	swapNums:
+		mov [aa + ecx], ebx; this is a=b
+		mov[aa + ecx + 4], eax; this is b = temp, where temp is techinically a
+		jmp inc_Counts;
+
+
+	b_Sort:
+		cmp loop1Count, 4;  loop to only go 4 times since array is of size 5 (0-4)
+		je display; display array
+
+		mov edx, 4; this seats up the stoping point for the check loop
+		sub edx, loop1Count; using bubble sort the second loo goes to n-i-1 
+		sub edx, 1;		where n is the array size, subtracted by i (value of first loop) amd then subbed by 1
+
+	check:; check if aa[i] is greater than aa[i+1] if it is then swap else increment values for check loop and continue
+		cmp loop2Count, edx; reachs end of loop go back to b_sort loop
+		je inc_Counts2;
+		mov eax, [aa + ecx]; temp
+		mov ebx, [aa + ecx + 4]; this is b
+
+		cmp eax, ebx; compare if it is greater 
+		jle inc_Counts;
+		jge swapNums;
+	
+	inc_Counts:; increment values ofr second check loop
+		inc loop2Count;
+		add ecx, 4;
+		jmp check;
+	
+	inc_Counts2:; updated values for loop 1 aka b_sort 
+		inc loop1Count;
+		mov loop2Count, 0;
+		mov ecx, 0;
+		jmp b_Sort;
+		
+	display:
+		call displayArray;
+	}*/
+
+	//part 3
+	// a./ total shirts
+	// b.) medium size shirts
+	// c.) short sleeve shirts
+	// d.) red shirts
+
+	//part a
 	_asm {
 		mov ecx, 0;
-		mov index, 0;
 
-	forLoopers:
-		add eax, 4;
-		cmp index, 5;
-		je donner;
+	loopi:
+		cmp arrayPos, 18;
+		je donney;
 
-		mov ebx, [aa + ecx];
-		mov element, ebx;
-		jmp displayy;
+		mov ebx, [aaa + ecx];
+		add total3DShirtCount, ebx;
 
-	displayy:
-		call displayArrayElement;
 		add ecx, 4;
-		inc index;
-		jmp forLoopers;
+		inc arrayPos;
+		jmp loopi;
 
-
-	donner:
+	donney:
+		call displayTotal3DCount;
 
 
 	}
 
+	//part b
+	_asm {
+		mov ecx, 8;
+		mov arrayLoc, 0;
+
+	loppi:
+		cmp arrayLoc, 3;
+		je doobi;
+
+		mov ebx, [aaa + ecx];
+		add total3DMediumCount, ebx;
+
+		mov ebx, [aaa + ecx + 4];
+		add total3DMediumCount, ebx;
+
+		mov eax, 8; 
+		imul countJump;
+
+		mov ecx, eax;
+		inc arrayLoc;
+		add countJump, 3;
+		jmp loppi;
+
+	doobi:
+		call displayTOtalMed;
+
+	}
 	return 0;
 }
